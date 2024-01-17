e2393784158b1eb28247fbd5f6d59c9b27810fcd
editpros22 <155381192+editpros22@users.noreply.github.com>
2024-01-18 00:40:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkPassword() {
     var password = document.getElementById("password").value;
 
     // Replace "your_password" with the actual password you want to use
-    if (password === "ateeb") {
+    if (password === "just wanna scream") {
         window.location.href = "nextindex.html";
     } else {
         alert("Incorrect password. Please try again.");
